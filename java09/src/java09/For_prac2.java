ca1f997d3776621d51ac093200011cad0c7feb3e
joongpyo <tlswndvy@gmail.com>
2023-09-08 21:44:02 +0900

    new For_prac2

--- java09/src/java09/For_prac2.java
+++ java09/src/java09/For_prac2.java
@@ -1,10 +1,17 @@
+/*
+   프로그램 명 : 5단 곱셈 프로그램
+*/
+
 package java09;
 
 public class For_prac2 {
 	public static void main(String[] args) {
-			for(int i=1;i<10;i++) {
-				System.out.println(i);
-			}
+		int i = 5;
+		int n;
+		for(n=1; n<10; n++) {
+			System.out.println(i+"*"+n+"="+(i*n));
+			
+		}
 	}
 
 }
